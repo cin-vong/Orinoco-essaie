538e5ab3e1a61ff2b70af1ff032ed47828b69b50
cin-vong <61393238+cin-vong@users.noreply.github.com>
2020-08-10 11:09:53 +0200

    01

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-//Variables
-const url= 'http://localhost:3000/api/teddies'
+//urlsearchparams
 
+const url = new URL('http://localhost:3000/api/teddies');
+const params = new URLSearchParams(url.search);
+params.set('baz', 3);
+
+params.has('baz') === true
+params.toString() === 'foo=1&bar=2&baz=3'
