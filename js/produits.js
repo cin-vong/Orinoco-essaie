b8ae5b1c09b09906badcf6e3a2a82de3aabd3416
cin-vong <61393238+cin-vong@users.noreply.github.com>
2020-09-12 21:00:00 +0200

    js.56466

--- js/produits.js
+++ js/produits.js
@@ -68,12 +68,60 @@ console.log(urlToFetch);
 
 getProduct(urlToFetch, 'colors');
 
+
+ //Select quantity//
+function quantity(productNumber){
+  const section = document.getElementById('teddies')
+  const formNumber = document.createElement('form')
+  const labelNumber = document.createElement('label')
+  labelNumber.setAttribute('for', 'selectNumber')
+  labelNumber.textContent = "Quantité : "
+  const selectNumber = document.createElement('select')
+  selectNumber.setAttribute('name', 'number')
+  selectNumber.id = "selectNumber"
+
+  section.appendChild(formNumber)
+  formNumber.appendChild(labelNumber)
+  formNumber.appendChild(selectNumber)
+ 
+  productNumber.forEach(productNumber => {
+   const option = document.createElement('option');
+   option.textContent = productNumber
+   productNumber.appendChild(option);
+
+   for (let i = 1; i < 5; i++){
+    const optionNumber= document.getElementById('option').innerHTML;
+    optionNumber.text = i
+    xNumber.add(optionNumber);
+    sessionStorage.setItem("number", "1")
+    number.addEventListener("click", function (event) {
+        sessionStorage.removeItem("number")
+        let amountStorage = xNumber.value
+        event.preventDefault();
+        sessionStorage.setItem("amount", amountStorage);
+    });
+   }
+ })
+}
+
+  //Send cart//
+
 function addToCart (product){
   addToCart (product);
-  const btn = document.createElement('div')
-  const div = document.createElement('div')
+  const btn = document.createElement('button')
   btn.innerHTML = `<div class= cart-btn>Ajouter au panier</div>`
-  div.classList.add('add-to-cart');
-  btn.appendChild(div);
+  div.classList.add('button');
+  btn.appendChild(form);
   btn.appendChild(btn);
+
+  btn.addEventListener('click',() => {
+    localStorage.setItem(product.name, JSON.stringify(product));
+    btn.classList.add('Panier')
+    div.textContent = 'Le produit a été ajouté au panier !'
+
+    const product = [product._id, product.name, product.price, product.imageUrl];
+  })
 }
+
+
+
